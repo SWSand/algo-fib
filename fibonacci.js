3d48788799fc775f1115d5bb11ba6154d933f7e4
Jose Sandoval <Software.Sandoval@gmail.com>
2024-01-31 14:42:50 -0600

    test before assignment test

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-  return 0;
+  let x = 1
+  let y = 0
+  let z = 0
+  
+  for(i=0; i<num; i++){
+    z = x+y
+    x = y
+    y = z
+  }
+  return y
 }
 
 module.exports = fibonacci;
